e248c28c681358126d79668eb223a679b9e6ba1b
Phong Le <Phong.le@epeinnovations.com>
2018-09-05 16:26:54 -0500

    Import layout module

--- src/app/layout/content-right/content-right.component.ts
+++ src/app/layout/content-right/content-right.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-content-right',
-  templateUrl: './content-right.component.html',
-  styleUrls: ['./content-right.component.css']
+  selector: "app-content-right",
+  templateUrl: "./content-right.component.html",
+  styleUrls: ["./content-right.component.css"]
 })
 export class ContentRightComponent implements OnInit {
 
