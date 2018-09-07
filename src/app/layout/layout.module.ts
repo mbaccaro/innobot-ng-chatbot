290bca5af4b5281de5176c83e4dbcb00ea91e2b7
Phong Le <Phong.le@epeinnovations.com>
2018-09-07 15:44:36 -0500

    Fullscreen layout

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 import { MainLayoutComponent } from "./main-layout.component";
 import { ContentLeftComponent } from "./content-left/content-left.component";
 import { ContentRightComponent } from "./content-right/content-right.component";
@@ -6,9 +7,11 @@ import { HeaderComponent } from "./header/header.component";
 import { MainContentComponent } from "./main-content/main-content.component";
 import { BreadcrumbModule} from "mcapp.ng.components";
 import { LayoutService } from "./layout.service";
+
 @NgModule({
     imports: [
-        BreadcrumbModule
+        BreadcrumbModule,
+        CommonModule
     ],
     declarations: [
         MainLayoutComponent,
