6465dbef2813f2418a1425a6c5b01f529c0c3029
Phong Le <Phong.le@epeinnovations.com>
2018-09-07 16:52:23 -0500

    Fix icons

--- src/app/shared/common/search-box/search-box.component.ts
+++ src/app/shared/common/search-box/search-box.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit, Output, Input, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-search-box',
-  templateUrl: './search-box.component.html',
-  styleUrls: ['./search-box.component.css']
+  selector: "app-search-box",
+  templateUrl: "./search-box.component.html",
+  styleUrls: ["./search-box.component.css"]
 })
 export class SearchBoxComponent implements OnInit {
+
   @Output() onSearch: EventEmitter<any> = new EventEmitter<any>();
   @Input() value: string;
+  @Input() placeholder = "Search...";
 
   constructor() { }
 
